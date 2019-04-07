68b63a132f7e312dbc1583fee0ed64a720f63c5d
Serega <level50elf@gmail.com>
2019-04-07 22:26:26 +0300

    completed sprute

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var { src, task, series, parallel, dest, watch } = require('gulp');
 var sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    rename = require('gulp-rename'),
+    del = require('del');
+
 
 
 function scss(cb) {
@@ -35,4 +38,54 @@ function css_stream(cb) {
 
 exports.scss = scss;
 exports.watch = parallel(watch_files, browser_sync);
-exports.default = exports.watch
+exports.default = exports.watch;
+
+
+
+
+
+// sprites////// _____--?.
+svgSprite = require('gulp-svg-sprite');
+
+function createSprite(cb) {
+    src('app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                css: { // Activate the «css» mode
+                    sprite: 'sprite.svg',
+                    render: {
+                        scss: {
+                            template: './templates/sprites.scss'
+                        }
+                    }
+                }
+            }
+        }))
+        .pipe(dest('./app/temp/sprite'));
+    cb()
+}
+
+function copySpriteCSS(cb) {
+    src('./app/temp/sprite/css/*.scss')
+        .pipe(rename('_sprite.scss'))
+        .pipe(dest('./app/assets/styles/modules/'))
+    cb()
+}
+
+function copySpriteGraphic(cb) {
+    src('./app/temp/sprite/css/**/*.svg')
+        .pipe(dest('./app/assets/images/sprites'))
+    cb()
+}
+
+function cleanSprite() {
+    return del(['./app/temp/sprite', './app/assets/images/sprites']);
+}
+
+function endCleanSprite() {
+    return del(['./app/temp/sprite']);
+}
+exports.cleanSprite = cleanSprite;
+exports.endCleanSprite = endCleanSprite;
+exports.sprites = series(createSprite, parallel(copySpriteGraphic, copySpriteCSS))
+//end sprites!
