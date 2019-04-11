f3f97dcdcabef71052c72b86a25070f5d1edb7e9
Serega <level50elf@gmail.com>
2019-04-11 03:08:38 +0300

    completed header

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: "App.js"
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
